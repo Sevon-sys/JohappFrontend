ba9f893ddd6d6841f14494d16510eccde47cbbcf
Andreas Hideblad <andreas@hideblad.se>
2021-03-04 16:39:03 +0100

    Solved table. Kind of...

--- History.js
+++ History.js
@@ -1,28 +1,39 @@
-const historyList = document.querySelector('.history-list')
+let historyList = document.querySelector('.history-list')
 
 historyBtn.onclick = (e) => {
     e.preventDefault();
-
+    clearTable()
     fetch('https://localhost:44399/Expenses', {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json'
         }
     }).then(repsonse => repsonse.json())
-    .then(data => {
-        let rows = data.map(x => createExpenseRow(x))
-        let divTable = document.getElementById('divTable')
-        rows.forEach(item => {
-            divTable.appendChild(item)
+        .then(data => {
+            console.log(data)
+            let rows = data.map(x => createExpenseRow(x))
+            rows.forEach(item => {
+                historyList.appendChild(item)
+            })
         })
-    })
-    
 }
 
-function createExpenseRow(data){
-    let row = document.createElement('tr')
-    .appendChild(
-        document.createElement('td')
-    )
-    return row;
+function addCellToRow(data, tr) {
+    const cell = tr.insertCell()
+    cell.textContent = data
 }
+
+function createExpenseRow(data) {
+    const tr = document.createElement('tr')
+    addCellToRow(data.name, tr)
+    addCellToRow(data.price, tr)
+    addCellToRow(data.date, tr)
+    addCellToRow(data.expensesCategories, tr)
+    return tr;
+}
+
+function clearTable() {
+    document.querySelectorAll('tr')
+        .forEach(x => x.remove())
+}
+
