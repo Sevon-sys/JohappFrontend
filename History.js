2342aa238561444fe961957e8f6a094d69cad9b1
Simon Karvonen <simonoskarkarvonen@gmail.com>
2021-03-05 13:25:48 +0100

    pågående sortering av datum

--- History.js
+++ History.js
@@ -1,6 +1,6 @@
 let historyList = document.querySelector('.history-list')
 
-historyBtn.onclick = (e) => {
+window.onload = (e) => {
     e.preventDefault();
     fetch('https://localhost:44399/Expenses', {
         method: 'GET',
@@ -15,7 +15,12 @@ historyBtn.onclick = (e) => {
             historyList.appendChild(item)
         })
     })
-    clearTable()
+    // clearTable()
+}
+
+function sortDates(data) {
+    const sortDate = data.slice().sort((a, b) => b.date - a.date)
+    return sortDate;
 }
 
 function addCellToRow(data, tr) {
@@ -38,3 +43,24 @@ function clearTable() {
     document.querySelectorAll('tr')
         .forEach(x => x.remove())
 }
+
+function convertDate(d) {
+    var str = d.split("/", 1)[0].trim()
+    var p = str.split("-")
+    return new Date(p[0] + "-"+p[1]+"-" + p[2]+"-"+p[3])
+  }
+
+  function sortByDate() {
+    var tbody = document.querySelector("#content2 tbody")
+    // get trs as array for ease of use
+    var rows = [].slice.call(tbody.querySelectorAll("tr"))
+
+    rows.sort(function(a, b) {
+      return convertDate(a.cells[3].innerHTML)})
+
+    rows.forEach(function(v) {
+      tbody.appendChild(v) // note that .appendChild() *moves* elements
+    });
+  }
+
+  document.querySelector('.sortBtn').addEventListener('click', sortByDate)
